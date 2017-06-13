402189d2266e7062a877759ed7171b96337491bb
markhorvath <markhorvath7@gmail.com>
2017-06-13 12:41:58 -0400

    initial commit, starting atlesson6

--- Lesson-2/5_MessageboardPartThree/MessageboardPartThree.py
+++ Lesson-2/5_MessageboardPartThree/MessageboardPartThree.py
@@ -39,8 +39,14 @@ class MessageHandler(BaseHTTPRequestHandler):
 
         # Store it in memory.
         memory.append(message)
+        print(message)
+        print(memory)
 
         # 1. Send a 303 redirect back to the root page.
+        self.send_response(303)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.send_header('Location', '/')
+        self.end_headers()
 
     def do_GET(self):
         # First, send a 200 OK response.
@@ -51,8 +57,10 @@ class MessageHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
         # 2. Put the response together out of the form and the stored messages.
+        newmsg = form.format("\n".join(memory))
 
         # 3. Send the response.
+        self.wfile.write(newmsg.encode())
 
 if __name__ == '__main__':
     server_address = ('', 8000)
